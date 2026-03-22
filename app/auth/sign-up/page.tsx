86a5681639a536189b4ccaeb31aa7bf012dd74af
pooYAYooq <pooyayooq@gmail.com>
2026-03-23 03:30:53 +1100

    feat: implement SignUpPage and AuthLayout components
    - Added SignUpPage component with form validation using react-hook-form and zod
    - Created AuthLayout component for consistent layout with navigation
    - Updated package.json to include necessary dependencies for form handling

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,7 +1,88 @@
+"use client";
+import { signUpSchema } from "@/app/schemas/auth";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+
 export default function SignUpPage() {
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="font-extrabold text-gray-700 text-7xl">Sign Up Page</h1>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Sign Up</CardTitle>
+        <CardDescription>
+          Fill in the form to create your account.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <FieldGroup className="gap-5">
+            <Controller
+              name="name"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field className="gap-y-2">
+                  <FieldLabel>Full Name</FieldLabel>
+                  <Input placeholder="John Doe" {...field} />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field className="gap-y-2">
+                  <FieldLabel>Email</FieldLabel>
+                  <Input placeholder="john@example.com" {...field} />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field className="gap-y-2">
+                  <FieldLabel>Password</FieldLabel>
+                  <Input type="password" placeholder="••••••••" {...field} />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Button type="submit" className="w-full">
+              Sign Up
+            </Button>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
   );
 }
