4f37b7abfe6ac61b01ce9c9e5090830138233928
pooYAYooq <pooyayooq@gmail.com>
2026-04-01 04:50:35 +1100

    feat: enhance blog post display with cards and links
    - Updated BlogPost component to use Card and CardContent for better UI.
    - Added image support for blog posts using Next.js Image component.
    - Implemented links for blog post titles to navigate to individual post pages.
    - Configured remote image patterns in next.config.ts for external images.

--- app/(app)/blog/page.tsx
+++ app/(app)/blog/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import { Card, CardContent } from "@/components/ui/card";
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function BlogPost() {
   const data = useQuery(api.posts.getPosts);
+
   return (
     <div className="py-12">
       <div className="text-center pb-12">
@@ -17,10 +21,25 @@ export default function BlogPost() {
       </div>
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         {data?.map((post) => (
-          <div key={post._id} className="p-4 border rounded-lg">
-            <h2 className="text-2xl font-bold">{post.title}</h2>
-            <p className="mt-2 text-gray-600">{post.body}</p>
-          </div>
+          <Card key={post._id}>
+            <div className=" relative h-48 w-full overflow-hidden">
+              <Image
+                src="https://w.wallhaven.cc/full/k7/wallhaven-k7k9j7.jpg"
+                alt="Blog Post Image"
+                fill
+                className="object-cover "
+              />
+            </div>
+
+            <CardContent>
+              <Link href={`/blog/${post._id}`} className="hover:underline">
+                <h1 className="text-2xl font-bold hover:text-primary">
+                  {post.title}
+                </h1>
+              </Link>
+              <p className="mt-2 text-gray-600">{post.body}</p>
+            </CardContent>
+          </Card>
         ))}
       </div>
     </div>
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        // https://w.wallhaven.cc/full/k7/wallhaven-k7k9j7.jpg
+        protocol: "https",
+        hostname: "w.wallhaven.cc",
+        port: "",
+        pathname: "/full/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
