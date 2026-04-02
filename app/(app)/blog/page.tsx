fa5066f0d6c9f069d625b55406cf139fe7f3a09c
pooYAYooq <pooyayooq@gmail.com>
2026-04-03 03:00:23 +1100

    feat: enhance blog post layout and update dependencies
    - Added CardFooter to blog post display for improved UX
    - Updated styles and layout for better responsiveness
    - Upgraded shadcn dependency to version 4.1.2
    - Introduced mcp.json for server configuration

--- app/(app)/blog/page.tsx
+++ app/(app)/blog/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { Card, CardContent } from "@/components/ui/card";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
 import Image from "next/image";
@@ -10,35 +12,49 @@ export default function BlogPost() {
   const data = useQuery(api.posts.getPosts);
 
   return (
-    <div className="py-12">
-      <div className="text-center pb-12">
-        <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl">
+    <div className="container mx-auto">
+      <div className="text-center py-12">
+        <h1 className="text-4xlxl font-extrabold tracking-tight sm:text-5xl">
           Our Blog
         </h1>
-        <p className="pt-4 max-w-2xl text-muted-foreground mx-auto text-xl">
+        <p className="pt-2 max-w-2xl text-muted-foreground mx-auto text-xl">
           Insight, thoughts, and stories from our universal team.
         </p>
       </div>
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid px-6 py-6 border-l border-r gap-6 md:grid-cols-2 lg:grid-cols-3">
         {data?.map((post) => (
-          <Card key={post._id}>
-            <div className=" relative h-48 w-full overflow-hidden">
+          <Card key={post._id} className="pt-0 gap-4">
+            <div className=" relative h-48 w-full overflow-hidden mb-8">
               <Image
                 src="https://w.wallhaven.cc/full/k7/wallhaven-k7k9j7.jpg"
                 alt="Blog Post Image"
                 fill
-                className="object-cover "
+                className="object-cover"
               />
             </div>
 
-            <CardContent>
-              <Link href={`/blog/${post._id}`} className="hover:underline">
-                <h1 className="text-2xl font-bold hover:text-primary">
+            <CardContent className="mb-0">
+              <Link href={`/blog/${post._id}`}>
+                <h1 className="text-xl mb-4 font-semibold hover:text-primary">
                   {post.title}
                 </h1>
               </Link>
-              <p className="mt-2 text-gray-600">{post.body}</p>
+              <p className="text-muted-foreground line-clamp-3">{post.body}</p>
             </CardContent>
+            <CardFooter>
+              <Link
+                className={cn(
+                  buttonVariants({
+                    variant: "link",
+                    className: "font-light",
+                  }),
+                  "px-0 py-0 text-muted-foreground hover:text-primary hover:no-underline",
+                )}
+                href={`/blog/${post._id}`}
+              >
+                Read More...
+              </Link>
+            </CardFooter>
           </Card>
         ))}
       </div>
